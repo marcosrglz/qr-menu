acb4586a855afebf3233ec6c4cfdaf190fd3bb33
marcosrglz <marcosrglez@gmail.com>
2023-07-31 13:08:30 +0200

    Añade la vista para ver las categorías y poder crearlas

--- gestion/views.py
+++ gestion/views.py
@@ -3,12 +3,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Count
 from django.urls import reverse_lazy
 from django.views import generic
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic import DetailView
 
 from core import models
 
 
-# Creación de menú
+# Menús
 class MenuCreateForm(forms.ModelForm):
     class Meta:
         model = models.Menu
@@ -36,6 +37,62 @@ class MenuCreateView(LoginRequiredMixin, CreateView):
         kwargs = super().get_form_kwargs()
         kwargs["user"] = self.request.user
         return kwargs
+
+
+class MenuEditView(LoginRequiredMixin, DetailView):
+    model = models.Menu
+    template_name = "gestion/editar_menu.html"
+    success_url = reverse_lazy("gestion:dashboard")
+    login_url = reverse_lazy("auth:login")
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["user"] = self.request.user
+        return kwargs
+
+    def get_queryset(self):
+        return models.Menu.objects.filter(usuario=self.request.user)
+
+
+# Categoría
+class CategoriaCreateForm(forms.ModelForm):
+    class Meta:
+        model = models.Categoria
+        fields = ["nombre"]
+
+    def __init__(self, *args, menu, **kwargs):
+        self.menu = menu
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        return models.Categoria.objects.create(
+            nombre=self.cleaned_data["nombre"],
+            menu=self.menu
+        )
+
+
+class CategoriaCreateView(LoginRequiredMixin, UpdateView):
+    model = models.Menu
+    form_class = CategoriaCreateForm
+    template_name = "gestion/crear_categoria.html"
+    login_url = reverse_lazy("auth:login")
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["instance"] = models.Categoria()
+        kwargs["menu"] = self.get_object()
+        return kwargs
+
+    def get_queryset(self):
+        return models.Menu.objects.filter(usuario=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        data["menu"] = self.get_object()
+        return data
+
+    def get_success_url(self):
+        return reverse_lazy("gestion:editar-menu", args=[self.get_object().pk])
 
 
 # Dashboard del usuario
